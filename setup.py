1681e7337b522633ccbb85a886a6c7fba38a5bfb
Stephen Stacha <sstacha@gmail.com>
2022-07-19 17:27:42 -0500

    correcting status to alpha and adding step for pip installing wheel

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(name='django-authgw',
       include_package_data=True,
       zip_safe=False,
       classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
